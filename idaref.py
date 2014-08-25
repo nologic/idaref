7024b0626be4efc84b90813a60a9c83666495642
Mikhail Sosonkin <mike@nanotick.net>
2014-08-25 16:23:20 +0100

    better organized

--- idaref.py
+++ idaref.py
@@ -2,61 +2,64 @@ import time, threading
 import sqlite3 as sq
 import os
 
-ref_term = False
-v = None
-db_dir = None
-inst_map = {}
+class InstructionReference:
+	def __init__(self):
+		self.ref_term = False
+		self.v = None
+		self.db_dir = None
+		self.inst_map = {}
+		self.last_inst = None
 
-def create(path="."):
-	global v
-	global db_dir
+	def create(self, path="."):
+		self.db_dir = path
+		dbpath = self.db_dir + os.sep + 'asm.sqlite'
 
-	db_dir = path
-	dbpath = db_dir + os.sep + 'asm.sqlite'
+		con = sq.connect(dbpath)
+		cur = con.cursor()
+		cur.execute("SELECT mnem, description FROM instructions")
+		con.commit()
 
-	con = sq.connect(dbpath)
-	cur = con.cursor()
-	cur.execute("SELECT mnem, description FROM instructions")
-	con.commit()
+		rows = cur.fetchall()
+		for row in rows:
+			inst = row[0]
+			lines = row[1].split("\n")
+			self.inst_map[inst] = lines
 
-	rows = cur.fetchall()
-	for row in rows:
-		inst = row[0]
-		lines = row[1].split("\n")
-		inst_map[inst] = lines
+		con.close()
+			
+		self.v = idaapi.simplecustviewer_t()
+		self.v.Create("Instruction Reference")
+		self.v.Show()
 
-	con.close()
+		threading.Timer(1, lambda: self.update()).start()
 
-	print len(inst_map)
-		
-	v = idaapi.simplecustviewer_t()
-	v.Create("Instruction Reference")
-	v.Show()
+	def update(self):
+		inst = GetMnem(ScreenEA()).upper()
 
-	threading.Timer(1, update).start()
+		if(inst != self.last_inst):
+			self.last_inst = inst
+			
+			self.v.ClearLines()
+			self.v.Refresh()
+			
+			if(inst in self.inst_map):
+				text = self.inst_map[inst]
 
-def update():
-	v.ClearLines()
-	v.Refresh
-	inst = GetMnem(ScreenEA())
+				for line in text:
+					line = line.encode('utf8')
+					self.v.AddLine(line)
 
-	text = inst_map[inst.upper()]
+			else:
+				self.v.AddLine(inst + " not documented.")
 
-	for line in text:
-		line = line.encode('utf8')
-		v.AddLine(line)
+			self.v.Refresh()
 
-	if(text == None):
-		v.AddLine(inst + " not documented.")
+		if(not self.ref_term):
+			threading.Timer(1, lambda: self.update()).start()
 
-	v.Refresh()
+	def terminate(self):
+		self.ref_term = True
 
-	if(not ref_term):
-		threading.Timer(1, update).start()
+ref = InstructionReference()
 
-def terminate():
-	global ref_term
-
-	ref_term = True
-
-print "Reference View loaded"
+print "Reference View loaded, ref:" + str(ref)
